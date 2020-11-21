019f4c2fb74f057d2fcabf3bdad8ca81a9670240
PTokarev <ptokarev@luxoft.com>
2020-11-21 11:18:28 +0300

    Step 4

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -7,6 +7,28 @@ package games;
 public class Slot {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int firstCounter = 1;
+        int secondCounter = 1;
+        int thirdCounter = 1;
+        int balance = 100;
+        int bet = 10;
+
+        do {
+            firstCounter = (firstCounter + (int) Math.round(Math.random() * 100)) % 7;
+            secondCounter = (secondCounter + (int) Math.round(Math.random() * 100)) % 7;
+            thirdCounter = (thirdCounter + (int) Math.round(Math.random() * 100)) % 7;
+
+            System.out.println("У Вас " + balance + "$, ставка - " + bet);
+            System.out.println("Крутим барабаны! Розыгрыш дал следующие результаты:");
+            balance -= bet;
+            System.out.println("Первый барабан - " + firstCounter + ", второй - " + secondCounter + ", третий - " + thirdCounter);
+
+            if (firstCounter == secondCounter && firstCounter == thirdCounter) {
+                balance += 1000;
+                System.out.println("Поздравляем! Вы выиграли 1000$ Ваш капитал теперь составляет: " + balance + "$\n");
+            } else {
+                System.out.println("Проигрыш " + bet + "$, Ваш капитал теперь составляет: " + balance + "$\n");
+            }
+        } while (balance >= bet);
     }
 }
